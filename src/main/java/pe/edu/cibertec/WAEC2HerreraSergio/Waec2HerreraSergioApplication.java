7f4ff27128e2ba20ecf88ebd0ca131134fd3a7fb
Sergio <serham_95@hotmail.com>
2024-04-26 17:35:57 -0500

    examen cl2 DAW

---
+++ src/main/java/pe/edu/cibertec/WAEC2HerreraSergio/Waec2HerreraSergioApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.WAEC2HerreraSergio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Waec2HerreraSergioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Waec2HerreraSergioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/cibertec/WAEC2HerreraSergio/Waec2HerreraSergioApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.cibertec.WAEC2HerreraSergio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Waec2HerreraSergioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
